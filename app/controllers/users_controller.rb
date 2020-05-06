9d63b0ad835e65df7f37849bdf7471c697709e5d
Daniel <danmoran.pro@gmail.com>
2020-05-06 15:56:21 -0600

    refracted show method and added get_followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 
 class UsersController < ApplicationController
   def show
-    repo = Faraday.get('https://api.github.com/users/margoflewelling/repos?page=1&per_page=5')
-    json = JSON.parse(repo.body, symbolize_names: true)
-    @user_repos = []
-    json.each do |repo|
-                reposit = Repo.new(repo[:name], repo[:html_url])
-                @user_repos << reposit
-              end
+    # repo = Faraday.get('https://api.github.com/users/margoflewelling/repos?page=1&per_page=5')
+    username = 'margoflewelling'
+    # binding.pry
+    conn(username)
+
+    @user_repos = get_repos
+
+    @user_followers = get_followers
+    # json = JSON.parse(repo.body, symbolize_names: true)
+    # @user_repos = []
+    # json.each do |repo|
+    #             reposit = Repo.new(repo[:name], repo[:html_url])
+    #             @user_repos << reposit
+    #           end
   end
 
   def new
@@ -31,5 +38,30 @@ class UsersController < ApplicationController
     params.require(:user).permit(:email, :first_name, :last_name, :password)
   end
 
+  def conn(username)
+    @connection = Faraday.new("https://api.github.com/users/#{username}")
+  end 
 
+  def get_repos
+    repos = @connection.get('repos?page=1&per_page=5')
+    json = JSON.parse(repos.body, symbolize_names: true)
+    @user_repos = []
+    json.each do |repo|
+                reposit = Repo.new(repo[:name], repo[:html_url])
+                @user_repos << reposit
+              end
+    @user_repos
+  end 
+
+  def get_followers
+    followers = @connection.get('followers')
+    json = JSON.parse(followers.body, symbolize_names: true)
+    @user_followers = []
+    json.each do |follower|
+                  @user_followers << Follower.new(follower[:login], follower[:html_url])
+              end
+    @user_followers
+  end 
+
+  
 end
