def0850ec6eccbc2ee50c2423008ab44ba359337
Daniel <danmoran.pro@gmail.com>
2020-05-06 15:56:54 -0600

    Test passing for showing users followers

---
+++ spec/features/user/user_can_see_followers_from_dashoard_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "As a registered user" do
+    it 'can add see their followers on dashboard' do
+      user = create(:user)
+  
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  
+      visit '/dashboard'
+      expect(page).to have_content("Github")
+     
+      within('.followers') do
+        expect(page).to have_link  ("danmoran-pro")
+        expect(page).to have_link  ("DavidTTran")
+        expect(page).to have_link  ("alex-latham")
+      end
+  end
+end
