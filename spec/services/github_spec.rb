dd7c83149380d7b3861c3233208a3541ea62cc43
Margo Flewelling <flewelling.margo@gmail.com>
2020-05-07 15:51:35 -0600

    Add github testing

--- spec/services/github_spec.rb
+++ spec/services/github_spec.rb
@@ -8,17 +8,30 @@ describe GithubService do
         repos = github.grab_repos
         expect(repos).to be_a Array
         repo1 = repos.first
-        # expect(repo1.name).to exists
+        expect(repo1.name).to_not eq(nil)
+        expect(repo1.url).to_not eq(nil)
       end
     end
 
     context '#grab_followers' do
       it "returns followers" do
+        github = GithubService.new
+        followers = github.grab_followers
+        expect(followers).to be_a Array
+        follower1 = followers.first
+        expect(follower1.name).to_not eq(nil)
+        expect(follower1.url).to_not eq(nil)
       end
     end
 
     context '#grab_following' do
       it "returns following" do
+        github = GithubService.new
+        following = github.grab_following
+        expect(following).to be_a Array
+        following1 = following.first
+        expect(following1.name).to_not eq(nil)
+        expect(following1.url).to_not eq(nil)
       end
     end
 
